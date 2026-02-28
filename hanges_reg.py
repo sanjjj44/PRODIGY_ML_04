d4d53c6900c84186a19590cab90942d7ea9643fc
SanjaiShankar_44 <sanjaishankar2005@gmail.com>
2026-02-28 20:47:10 +0530

    Add files via upload

---
+++ hanges_reg.py
@@ -0,0 +1,44 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+IMG_SIZE = 64
+DATA_DIR = "data"
+X = []
+y = []
+class_names = []
+for idx, folder in enumerate(os.listdir(DATA_DIR)):
+    class_names.append(folder)
+    folder_path = os.path.join(DATA_DIR, folder)
+    for img_name in os.listdir(folder_path):
+        img_path = os.path.join(folder_path, img_name)
+        img = cv2.imread(img_path)
+        img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+        X.append(img)
+        y.append(idx)
+X = np.array(X) / 255.0
+y = to_categorical(y)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+model = Sequential([
+    Conv2D(32, (3,3), activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 3)),
+    MaxPooling2D(2,2),
+    Conv2D(64, (3,3), activation='relu'),
+    MaxPooling2D(2,2),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dense(len(class_names), activation='softmax')
+])
+model.compile(
+    optimizer='adam',
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+model.fit(X_train, y_train, epochs=15, validation_data=(X_test, y_test))
+loss, accuracy = model.evaluate(X_test, y_test)
+print("Test Accuracy:", accuracy)
